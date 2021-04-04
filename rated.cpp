b2574a82529881d20ad1408e2285a12c604dc743
The One <rahulsharma22459@gmail.com>
2021-04-04 09:45:27 +0530

    codeforces.com/contest/1505/problem/A

---
+++ rated.cpp
@@ -0,0 +1,57 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string q;
+	while(true)
+	{
+		
+			getline(cin,q);
+			if(q.empty())
+			{
+			    break;
+		    }
+		cout<<"no\n"<<flush;
+	}
+}
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string q;
+	while(true)
+	{
+		try
+		{
+			getline(cin,q);
+			if(q.empty())
+			{
+				throw 1;
+			}
+		}
+		catch(int x)
+		{
+			break;
+		}
+		cout<<"no\n"<<flush;
+	}
+}
+
+
+/* try
+{
+	throw exception;
+}
+catch(type varliable)
+{
+	
+}*/
+/*
+Is it rated?
+Is it rated?
+Is it rated?
+Is it rated?
+
+Is it rated?
+Is it rated?*/
